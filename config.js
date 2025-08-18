d7e962a6a78fac72ab54b12017a8d11159d7ccfe
beijingshunyi <2700768701@qq.com>
2025-08-18 13:56:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // 配置文件
-    const config = {
-        // 已设置为你提供的API地址
-        apiUrl: 'https://telegram-bot.jbk123jbk.workers.dev/api'
-    };
-    
+const config = {
+    // 已设置为您的API地址
+    apiUrl: 'https://telegram-bot.jbk123jbk.workers.dev',
+    adminKey: '9712202273aA.' // 添加管理员密钥
+};
